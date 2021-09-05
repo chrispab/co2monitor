0648034580b90d72acaa393fc4f9ff4a0608b648
chrispab <cbattisson@gmail.com>
2021-09-05 23:05:44 +0100

    secrets

---
+++ src/secrets_example.h
@@ -0,0 +1,9 @@
+//work
+const char* ssid_1 = "BTF_Staffdfdf";
+const char* password_1 = "password!";
+//home
+const char* ssid_2 = "nodfdfdfork";
+const char* password_2 = "password";
+
+const char* soft_ap_ssid = "CO2 Monitor";
+const char* soft_ap_password = "password";
