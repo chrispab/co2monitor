01cda02727de3779e15bb4228d38f052bfdcce12
chrispab <cbattisson@gmail.com>
2021-09-13 17:50:26 +0100

    0 co2 fix?

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@
 #include "MHZ19.h"
 // #define RX_PIN 10                                          // Rx pin which the MHZ19 Tx pin is attached to
 // #define TX_PIN 11                                          // Tx pin which the MHZ19 Rx pin is attached to
-#define MHZ19_BAUDRATE 9600 // Device to MH-Z19 Serial baudrate (should not be changed)
+#define MHZ19_BAUDRATE 9600  // Device to MH-Z19 Serial baudrate (should not be changed)
 
 //!The pins used by ESP32 are defined in this way:
 // #define RX_PIN 33 // Rx pin which the MHZ19 Tx pin is attached to
@@ -22,14 +22,14 @@
 // int dhtPin = 4;
 
 //!The pins used by ESP32 are defined in this way:
-#define RX_PIN 16 // Rx pin which the MHZ19 Tx pin is attached to
-#define TX_PIN 17 // Tx pin which the MHZ19 Rx pin is attached to
+#define RX_PIN 16  // Rx pin which the MHZ19 Tx pin is attached to
+#define TX_PIN 17  // Tx pin which the MHZ19 Rx pin is attached to
 // int dhtPin = 4;
 
-MHZ19 myMHZ19; // Constructor for library
+MHZ19 myMHZ19;  // Constructor for library
 // SoftwareSerial mySerial;  // (Uno example) create device to MH-Z19 serial
 
-#define ESP32_ONBOARD_BLUE_LED_PIN GPIO_NUM_2 // RHS_P_4 esp32 devkit on board blue LED
+#define ESP32_ONBOARD_BLUE_LED_PIN GPIO_NUM_2  // RHS_P_4 esp32 devkit on board blue LED
 #define HEART_BEAT_TIME 500
 LedFader heartBeatLED(ESP32_ONBOARD_BLUE_LED_PIN, 1, 0, 255, HEART_BEAT_TIME, true);
 
@@ -76,22 +76,19 @@ DHTesp dht22;
 const char *remoteServerIP = "192.168.0.199";
 
 // const char *serverName = "http://192.168.0.199:8080/post-data.php"; // work hp @ home wifi
-const char *serverName = "http://dotty.dynu.com:8080/post-data.php"; // work hp @ home wifi
+const char *serverName = "http://dotty.dynu.com:8080/post-data.php";  // work hp @ home wifi
 
 // Keep this API Key value to be compatible with the PHP code provided in the project page.
 // If you change the apiKeyValue value, the PHP file /post-data.php also needs to have the same key
 String apiKeyValue = "tPmAT5Ab3j7F9";
 
-boolean try_wifi_connect(const char *ssid, const char *password)
-{
+boolean try_wifi_connect(const char *ssid, const char *password) {
     WiFi.begin(ssid, password);
     //try to connect for 5 secs
     // boolean timedOut = false;
     int counter = 0;
-    while ((WiFi.status() != WL_CONNECTED))
-    {
-        if (counter == 20)
-        {
+    while ((WiFi.status() != WL_CONNECTED)) {
+        if (counter == 20) {
             Serial.print("could not connect to :");
             Serial.println(ssid);
             return false;
@@ -100,15 +97,14 @@ boolean try_wifi_connect(const char *ssid, const char *password)
         Serial.print("trying to connect to :");
         Serial.println(ssid);
 
-        delay(500); //wait, it might still connect
+        delay(500);  //wait, it might still connect
     }
     Serial.print("connected to :");
     Serial.println(ssid);
     return true;
 }
 
-void setup_wifi()
-{
+void setup_wifi() {
     delay(10);
     // We start by connecting to a WiFi network
     // Serial.println();
@@ -123,16 +119,14 @@ void setup_wifi()
     //try work wifi first
     boolean connected = false;
     connected = try_wifi_connect(work_ssid, work_password);
-    if (!connected)
-    {
+    if (!connected) {
         connected = try_wifi_connect(home_ssid, home_password);
     }
-    if (!connected)
-    {
-        return; // restart device
+    if (!connected) {
+        return;  // restart device
     }
 
-    randomSeed(micros()); //??
+    randomSeed(micros());  //??
 
     Serial.print("ESP32 IP as soft AP: ");
     Serial.println(WiFi.softAPIP());
@@ -146,13 +140,11 @@ void setup_wifi()
 }
 
 // MQTT start
-void callback(char *topic, byte *payload, unsigned int length)
-{
+void callback(char *topic, byte *payload, unsigned int length) {
     Serial.print("Message arrived [");
     Serial.print(topic);
     Serial.print("] ");
-    for (int i = 0; i < length; i++)
-    {
+    for (int i = 0; i < length; i++) {
         Serial.print((char)payload[i]);
     }
     Serial.println();
@@ -167,29 +159,24 @@ void callback(char *topic, byte *payload, unsigned int length)
     // }
 }
 
-void reconnectMQTT()
-{
+void reconnectMQTT() {
     // Loop until we're reconnected or timed out
     // try for 3 secs to connect
     boolean timedOut = false;
     int startTime = millis();
-    while (!MQTTclient.connected() && !timedOut)
-    {
+    while (!MQTTclient.connected() && !timedOut) {
         Serial.print("Attempting MQTT connection...");
         // Create a random client ID
         String clientId = "ESP8266Client-";
         clientId += String(random(0xffff), HEX);
         // Attempt to connect
-        if (MQTTclient.connect(clientId.c_str()))
-        {
+        if (MQTTclient.connect(clientId.c_str())) {
             Serial.println("connected to MQTT server");
             // Once connected, publish an announcement...
             MQTTclient.publish("co2sensor", "hello co2");
             // ... and resubscribe
             // MQTTclient.subscribe("inTopic");
-        }
-        else
-        {
+        } else {
             Serial.print("failed to connect to MQTT server, rc=");
             Serial.print(MQTTclient.state());
             Serial.println(" try again in 1 seconds");
@@ -202,65 +189,66 @@ void reconnectMQTT()
 }
 //MQTT end
 
-String readCO2Sensor()
-{
-    int CO2ppm = myMHZ19.getCO2(); // Request CO2 (as ppm)
+//read co2 sensor and return as string
+int lastGoodCO2ppm;
+
+String readCO2Sensor() {
+    int CO2ppm = myMHZ19.getCO2();  // Request CO2 (as ppm)
     // float p = bme.readPressure() / 100.0F;
-    if (CO2ppm == 0)
-    { //! dont send back 0 - use prev or get till a number
-        Serial.println("!!!!!!     Failed to read from CO2 sensor!");
+    if (CO2ppm == 0) {  //! dont send back 0 - use prev or get till a number
+        Serial.println("!!!!!!     Failed to read from CO2 sensor! 0 read");
+
         //try restarting the serial i/f to sensor
         Serial2.begin(9600);
 
         // myMHZ19.begin(mySerial);  // *Serial(Stream) refence must be passed to library begin().
-        myMHZ19.begin(Serial2); // *Serial(Stream) refence must be passed to library begin().
+        myMHZ19.begin(Serial2);  // *Serial(Stream) refence must be passed to library begin().
         CO2ppm = myMHZ19.getCO2();
-        return String(CO2ppm);
-    }
-    else
-    {
+
+        if (CO2ppm > 0) {
+            return String(CO2ppm);
+        } else {  //return previous good reading made
+            return String(lastGoodCO2ppm);
+        }
+    } else {
         // Serial.print("Sensor reads : ");
         // Serial.println(CO2ppm);
+        lastGoodCO2ppm = CO2ppm;
         return String(CO2ppm);
     }
 }
 #define FORMAT_SPIFFS_IF_FAILED true
 
-void setup()
-{
+void setup() {
     delay(2000);
     heartBeatLED.begin();
     dht22.setup(dhtPin, DHTesp::DHT22);
-    Serial.begin(MONITOR_SPEED); // Device to serial monitor feedback
+    Serial.begin(MONITOR_SPEED);  // Device to serial monitor feedback
 
     // Initialize SPIFFS
-    if (!SPIFFS.begin(FORMAT_SPIFFS_IF_FAILED))
-    {
+    if (!SPIFFS.begin(FORMAT_SPIFFS_IF_FAILED)) {
         Serial.println("An Error has occurred while mounting SPIFFS");
         return;
     }
 
+    // sofserial setup if using
     // mySerial.begin(MHZ19_BAUDRATE, SWSERIAL_8N1, RX_PIN, TX_PIN);
     // mySerial.enableIntTx(false);
-
     // mySerial.begin(MHZ19_BAUDRATE);
     // myMHZ19.printCommunication();                            // Error Codes are also included here if found (mainly for debugging/interest)
-    // (Uno example) device to MH-Z19 serial start
 
-    // Serial2.begin(9600, SERIAL_8N1);
     Serial2.begin(9600);
 
     // myMHZ19.begin(mySerial);  // *Serial(Stream) refence must be passed to library begin().
-    myMHZ19.begin(Serial2); // *Serial(Stream) refence must be passed to library begin().
+    myMHZ19.begin(Serial2);  // *Serial(Stream) refence must be passed to library begin().
 
-    myMHZ19.autoCalibration(); // Turn auto calibration ON (OFF autoCalibration(false))
+    myMHZ19.autoCalibration();  // Turn auto calibration ON (OFF autoCalibration(false))
 
     char myVersion[4];
     myMHZ19.getVersion(myVersion);
 
     Serial.print("\nFirmware Version: ");
-    for (byte i = 0; i < 4; i++)
-    {
+    for (byte i = 0; i < 4; i++) {
         Serial.print(myVersion[i]);
         if (i == 1)
             Serial.print(".");
@@ -281,8 +269,7 @@ void setup()
     setup_wifi();
 
     //ONLY use mqtt if at home
-    if (WiFi.SSID() == home_ssid)
-    {
+    if (WiFi.SSID() == home_ssid) {
         MQTTclient.setServer(mqtt_server, 1883);
         MQTTclient.setCallback(callback);
     }
@@ -298,40 +285,30 @@ void setup()
 
     // getDataTimer = millis() - 11000;
     // Route for root / web page
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-              { request->send(SPIFFS, "/index.html"); });
-    server.on("/co2", HTTP_GET, [](AsyncWebServerRequest *request)
-              {
-                  request->send_P(200, "text/plain", readCO2Sensor().c_str());
-                  Serial.println("temperature readCO2Sensor() called from ajax");
-              });
-    server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request)
-              {
-                  float temperature = dht22.getTemperature();
-                  request->send(200, "text/plain", String(temperature));
-              });
-    server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request)
-              {
-                  float humidity = dht22.getHumidity();
-                  request->send(200, "text/plain", String(humidity));
-              });
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) { request->send(SPIFFS, "/index.html"); });
+    server.on("/co2", HTTP_GET, [](AsyncWebServerRequest *request) {
+        request->send_P(200, "text/plain", readCO2Sensor().c_str());
+        Serial.println("readCO2Sensor() called from ajax");
+    });
+    server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request) {
+        float temperature = dht22.getTemperature();
+        request->send(200, "text/plain", String(temperature));
+    });
+    server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request) {
+        float humidity = dht22.getHumidity();
+        request->send(200, "text/plain", String(humidity));
+    });
 
-    // server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    //     request->send(200, "text/plain", "Hello, world");
-    // });
-    server.on("/favicon.ico", HTTP_GET, [](AsyncWebServerRequest *request)
-              { request->send(SPIFFS, "/favicon-32x32.png", "image/png"); });
+    server.on("/favicon.ico", HTTP_GET, [](AsyncWebServerRequest *request) { request->send(SPIFFS, "/favicon-32x32.png", "image/png"); });
     // Start server
     Serial.println("Starting CO2 monitor Web Server...");
     server.begin();
     Serial.println("CO2 monitor Web Server Started");
 }
 
-void postDataToRemoteDB(int CO2, float Temperature, float Humidity)
-{
+void postDataToRemoteDB(int CO2, float Temperature, float Humidity) {
     //Check WiFi connection status
-    if (WiFi.status() == WL_CONNECTED)
-    {
+    if (WiFi.status() == WL_CONNECTED) {
         WiFiClient client;
         HTTPClient http;
 
@@ -344,7 +321,7 @@ void postDataToRemoteDB(int CO2, float Temperature, float Humidity)
         //! trim t, h to 1dp!!
         // Prepare your HTTP POST request data
         // String httpRequestData = "api_key=" + apiKeyValue + "&value1=" + String(CO2) + "&value2=" + String(Temperature) + "&value3=" + String(Humidity) + "";
-        String httpRequestData = "api_key=" + apiKeyValue + "&value1=" + String(CO2) + "&value2=" + String(Temperature,1) + "&value3=" + String(Humidity,1);
+        String httpRequestData = "api_key=" + apiKeyValue + "&value1=" + String(CO2) + "&value2=" + String(Temperature, 1) + "&value3=" + String(Humidity, 1);
         // Serial.println(String(Temperature, 1));
 
         Serial.print("remote server");
@@ -368,21 +345,16 @@ void postDataToRemoteDB(int CO2, float Temperature, float Humidity)
         //http.addHeader("Content-Type", "application/json");
         //int httpResponseCode = http.POST("{\"value1\":\"19\",\"value2\":\"67\",\"value3\":\"78\"}");
 
-        if (httpResponseCode > 0)
-        {
+        if (httpResponseCode > 0) {
             Serial.print("HTTP Response code: ");
             Serial.println(httpResponseCode);
-        }
-        else
-        {
+        } else {
             Serial.print("Error code: ");
             Serial.println(httpResponseCode);
         }
         // Free resources
         http.end();
-    }
-    else
-    {
+    } else {
         Serial.println("WiFi Disconnected");
     }
 }
@@ -396,17 +368,15 @@ unsigned long previousTime = 0;
 // Define timeout time in milliseconds (example: 2000ms = 2s)
 const long timeoutTime = 2000;
 
-void loop()
-{
+void loop() {
     heartBeatLED.update();
 
-    if (millis() - getDataTimer >= 15000)
-    { //get data every n ms
+    if (millis() - getDataTimer >= 15000) {  //get data every n ms
         /* note: getCO2() default is command "CO2 Unlimited". This returns the correct CO2 reading even 
         if below background CO2 levels or above range (useful to validate sensor). You can use the 
         usual documented command with getCO2(false) */
 
-        CO2 = myMHZ19.getCO2(); // Request CO2 (as ppm)
+        CO2 = readCO2Sensor().toInt();;  // Request CO2 (as ppm)
 
         Serial.println("Reading Sensors:");
         Serial.print("CO2 (ppm): ");
@@ -416,27 +386,22 @@ void loop()
         Serial.print("Humidity (C): ");
         Serial.println(dht22.getHumidity());
 
-        if (WiFi.status() != WL_CONNECTED)
-        {
+        if (WiFi.status() != WL_CONNECTED) {
             Serial.println("WiFi is NOT connected - trying wifi_setup()");
             setup_wifi();
         }
-        if (WiFi.SSID() == home_ssid)
-        {
-            if (!MQTTclient.connected())
-            {
+        if (WiFi.SSID() == home_ssid) {
+            if (!MQTTclient.connected()) {
                 reconnectMQTT();
             }
 
             snprintf(msg, MSG_BUFFER_SIZE, "%d", CO2);
             Serial.print("MQTT Publish message: co2sensor_01/co2ppm : ");
             Serial.println(msg);
-            if (MQTTclient.connected())
-            {
+            if (MQTTclient.connected()) {
                 MQTTclient.loop();
                 MQTTclient.publish("co2sensor_01/co2ppm", msg);
-            }
-            else
+            } else
                 Serial.println("MQTT NOT connected - cant publish CO2");
         }
         //send the data to the remote db to store
@@ -446,4 +411,5 @@ void loop()
     }
 }
 
+//to upload /data folder to device SPIFFS
 // pio run -t uploadfs
